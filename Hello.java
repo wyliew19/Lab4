2a0e9aa3da3d569539713d7ee519d824b85129d7
Will Wylie <wyliew@umich.edu>
2023-02-09 20:36:29 -0500

    Committing changes

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     for(int i=1;i<=10;i++) {
       System.out.println("Hello World..."+i);
+      System.out.println("Testing Jenkins");
     }
   }
 }
